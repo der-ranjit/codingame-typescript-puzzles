773e93828e19e89f576221b1095a2e078e73f724
Ranjit Mevius <ranjit.mevius@outlook.com>
2022-08-21 03:35:43 +0200

    update texts

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-/** Import the puzzle code you want to use and comment-out all other puzzles */
+/** Import the puzzle class you want to use and comment-out all other puzzles */
 
 import { ShadowKnightEp1 as Puzzle } from "./codingame-puzzles/shadow-of-the-knight-episode-1/solution";
 //  import { SomeOtherPuzzle as Puzzle } from "./codingame-puzzles/some-other-puzzle/solution";
