0013f5c4d569995372d5ade84a377a58d3c1c6b2
Ranjit Mevius <ranjit.mevius@outlook.com>
2022-08-23 01:39:54 +0200

    create vscode snippet to easily create PuzzleSolver class

---
+++ src/codingame-puzzles/mars-lander-episode-2/MarsLander1Solver.ts
@@ -0,0 +1,23 @@
+import { InitialGameInput, PuzzleSolver, StepwiseGameInput } from "../PuzzleSolver";
+
+interface MarsLander1InitialInput extends InitialGameInput {
+
+}
+
+interface MarsLander1StepwiseInput extends StepwiseGameInput {
+
+}
+
+export class MarsLander1Solver extends PuzzleSolver<MarsLander1InitialInput, MarsLander1StepwiseInput> {
+    protected parseInitialGameInput(): MarsLander1InitialInput {
+        return {};
+    }
+
+    protected parseStepwiseGameInput(stepwiseCGInput: string): MarsLander1StepwiseInput {
+        return {};
+    }
+
+    protected getSolutionForNextStep(stepwiseInput: MarsLander1StepwiseInput): string {
+        return "";
+    }
+}
