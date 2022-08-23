9413e7879f5544943c76c23d66451753228a8155
Ranjit Mevius <ranjit.mevius@outlook.com>
2022-08-23 15:22:50 +0200

    test commit

--- src/codingame-puzzles/mars-lander-episode-1/MarsLander1Solver.ts
+++ src/codingame-puzzles/mars-lander-episode-1/MarsLander1Solver.ts
@@ -11,6 +11,7 @@ interface MarsLander1StepwiseInput extends StepwiseGameInput {
 export class MarsLander1Solver extends PuzzleSolver<MarsLander1InitialInput, MarsLander1StepwiseInput> {
     protected parseInitialGameInput(): MarsLander1InitialInput {
         return {};
+        // gewa
     }
 
     protected parseStepwiseGameInput(stepwiseCGInput: string): MarsLander1StepwiseInput {
