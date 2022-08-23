4f5d309f23cf380dfe7417581765035bead07ba1
Ranjit Mevius <ranjit.mevius@outlook.com>
2022-08-23 15:25:17 +0200

    Revert "test"
    This reverts commit b4220bba85b549ab0cf8798c8ffc46fa37d0f7b7.

--- src/codingame-puzzles/mars-lander-episode-1/MarsLander1Solver.ts
+++ src/codingame-puzzles/mars-lander-episode-1/MarsLander1Solver.ts
@@ -12,7 +12,6 @@ export class MarsLander1Solver extends PuzzleSolver<MarsLander1InitialInput, Mar
     protected parseInitialGameInput(): MarsLander1InitialInput {
         return {};
         // gewa
-        //wqgq
     }
 
     protected parseStepwiseGameInput(stepwiseCGInput: string): MarsLander1StepwiseInput {
